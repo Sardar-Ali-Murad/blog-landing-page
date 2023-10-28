0624934b54e541fe8485684c07db8ba3f109e62f
Sardar-Ali-Murad <mar389786@gmail.com>
2023-10-28 20:15:02 +0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#e5e7eb] h-[100px] flex flex-row justify-center items-center">
+      <p>© 2023 Zolla. By Murad</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex flex-row justify-center items-center h-[300px] bg-gray-600">
+      <h1 className="text-white text-[14px] md:text-[20px] lg:text-[30px]">Zolla - Blog Landing Page Tailwind CSS  HTML</h1>
+    </div>
+  );
+};
+
+export default Navbar;
